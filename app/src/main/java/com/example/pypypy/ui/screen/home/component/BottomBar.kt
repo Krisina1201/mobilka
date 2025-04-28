0418437b98f2f6e6e6f2c4f0e74e6c2f8ca2e934
Diana <hiew@mail.ru>
2025-04-28 16:26:21 +0300

    add main screen

--- app/src/main/java/com/example/pypypy/ui/screen/home/component/BottomBar.kt
+++ app/src/main/java/com/example/pypypy/ui/screen/home/component/BottomBar.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,12 +25,14 @@ import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.pypypy.R
+import com.example.pypypy.ui.theme.MatuleTheme
 
 @Composable
 fun BottomBar() {
     Box(
-        modifier = Modifier.fillMaxWidth(),
-        contentAlignment = Alignment.BottomCenter
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(136.dp)
     ) {
         Image(
             painter = painterResource(R.drawable.vector_1789),
@@ -40,46 +44,43 @@ fun BottomBar() {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 38.dp)
-                .padding(horizontal = 24.dp),
-            verticalAlignment = Alignment.Top
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 30.dp, start = 20.dp, end = 24.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                modifier = Modifier.weight(1f, fill = false).padding(end = 40.dp),
-                horizontalArrangement = Arrangement.spacedBy(40.dp, Alignment.Start)
-            ) {
-                IconButton(onClick = {  }) {
+            Row(horizontalArrangement = Arrangement.spacedBy(42.dp)) {
+                IconButton(onClick = {}) {
                     Image(painterResource(R.drawable.home), "Дом", Modifier.size(24.dp))
                 }
-                IconButton(onClick = {  }) {
+                IconButton(onClick = {}) {
                     Image(painterResource(R.drawable.heart), "Избранное", Modifier.size(24.dp))
                 }
             }
 
             Column(
+                verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
+                modifier = Modifier.padding(bottom = 30.dp)
             ) {
                 IconButton(
-                    onClick = { /* ... */ },
-                    modifier = Modifier.size(32.dp)
+                    onClick = {},
+                    modifier = Modifier
+                        .size(56.dp)
+                        .background(
+                            MatuleTheme.colors.accent,
+                            shape = RoundedCornerShape(percent = 50)
+                        )
                 ) {
-                    Image(
-                        painter = painterResource(R.drawable.shop),
-                        contentDescription = "Корзина",
-                        modifier = Modifier.size(48.dp)
-                    )
+                    Image(painterResource(R.drawable.bag_2), "Корзина")
                 }
             }
 
-            Row(
-                modifier = Modifier.weight(1f, fill = false).padding(start = 30.dp),
-                horizontalArrangement = Arrangement.spacedBy(40.dp, Alignment.End)
-            ) {
-                IconButton(onClick = {  }) {
+            Row(horizontalArrangement = Arrangement.spacedBy(40.dp)) {
+                IconButton(onClick = {}) {
                     Image(painterResource(R.drawable.colocol), "Уведомления", Modifier.size(24.dp))
                 }
-                IconButton(onClick = {  }) {
+                IconButton(onClick = {}) {
                     Image(painterResource(R.drawable.people), "Профиль", Modifier.size(24.dp))
                 }
             }
--- app/src/main/java/com/example/pypypy/ui/screen/home/home/HomeSceen.kt
+++ app/src/main/java/com/example/pypypy/ui/screen/home/home/HomeSceen.kt
@@ -1,9 +1,11 @@
 package com.example.pypypy.ui.screen.home.home
 
+import android.widget.ImageButton
 import androidx.compose.material3.IconButton
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -17,6 +19,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -28,6 +31,7 @@ import com.example.pypypy.ui.screen.home.popylar.PopularScreenContent
 import com.example.pypypy.ui.screen.home.popylar.PopylarSneakersViewModel
 import org.koin.compose.viewmodel.koinViewModel
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -36,10 +40,12 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -73,120 +79,153 @@ fun HomeScreenContent(
 ) {
     val message = remember{mutableStateOf("")}
 
-    Row(
-        modifier = Modifier.padding(paddingValues)
-            .fillMaxWidth()
-            .padding(start = 20.dp, end = 15.dp, top = 30.dp)
+    Column(
+        modifier = Modifier.padding(paddingValues = paddingValues).verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        TextField(
-            value = message.value,
-            onValueChange = {newText -> message.value = newText},
-            placeholder = { Text("Поиск") },
+        Row(
             modifier = Modifier
-                .weight(1f)
-                .requiredHeight(52.dp),
-            leadingIcon = {Icon(painterResource(R.drawable.check), contentDescription = "Поиск")},
-            shape = RoundedCornerShape(12.dp),
-            colors = TextFieldDefaults.colors(
-                focusedContainerColor = MatuleTheme.colors.block,
-                disabledContainerColor = MatuleTheme.colors.background,
-                unfocusedContainerColor = MatuleTheme.colors.background,
-                errorContainerColor = MatuleTheme.colors.background,
-                unfocusedIndicatorColor = Color.Transparent,
-                focusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent,
-                errorIndicatorColor = Color.Red
-            )
-        )
-        IconButton(
-            onClick = {  },
-            modifier = Modifier
-                .size(52.dp)
-                .background(
-                    MatuleTheme.colors.accent,
-                    shape = RoundedCornerShape(percent = 50)
-                )
+                .fillMaxWidth()
+                .padding(start = 20.dp, end = 15.dp, top = 35.dp)
         ) {
-            Image(painterResource(R.drawable.sliders), "Сортировка")
+            TextField(
+                value = message.value,
+                onValueChange = {newText -> message.value = newText},
+                placeholder = { Text("Поиск") },
+                modifier = Modifier
+                    .weight(1f)
+                    .requiredHeight(52.dp),
+                leadingIcon = {Icon(painterResource(R.drawable.check), contentDescription = "Поиск")},
+                shape = RoundedCornerShape(12.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MatuleTheme.colors.block,
+                    disabledContainerColor = MatuleTheme.colors.background,
+                    unfocusedContainerColor = MatuleTheme.colors.background,
+                    errorContainerColor = MatuleTheme.colors.background,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    errorIndicatorColor = Color.Red
+                )
+            )
+            IconButton(
+                onClick = {  },
+                modifier = Modifier
+                    .size(52.dp)
+                    .background(
+                        MatuleTheme.colors.accent,
+                        shape = RoundedCornerShape(percent = 50)
+                    )
+            ) {
+                Image(painterResource(R.drawable.sliders), "Сортировка")
+            }
+
         }
 
-    }
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp).padding(bottom = 24.dp)
+        ) {
+            Text(text = "Категории",
+                fontWeight = FontWeight.Medium,
+                fontSize = 16.sp, modifier = Modifier.padding(bottom = 16.dp, top = 24.dp))
 
-    Column(
-        modifier = Modifier.padding(paddingValues).fillMaxWidth().padding(vertical = 100.dp, horizontal = 20.dp)
-    ) {
-        Text(text = "Категории",
-            fontWeight = FontWeight.Medium,
-            fontSize = 16.sp, modifier = Modifier.padding(bottom = 16.dp, top = 6.dp))
+            val langs = listOf("Всё", "Outdoor", "Tennis", "Podcrdyli")
 
-        val langs = listOf("Всё", "Outdoor", "Tennis", "Podcrdyli")
-
-        LazyRow {
-            items(langs) { lang ->
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .size(width = 108.dp, height = 40.dp)
-                        .padding(end = 16.dp),
-                    shape = RoundedCornerShape(10.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.Black,
-                        containerColor = Color.White
-                    )
-                ){
-                    Text(text = lang, fontWeight = FontWeight.Normal, fontSize = 12.sp)
+            LazyRow {
+                items(langs) { lang ->
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .size(width = 108.dp, height = 40.dp)
+                            .padding(end = 16.dp),
+                        shape = RoundedCornerShape(10.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            contentColor = Color.Black,
+                            containerColor = Color.White
+                        )
+                    ){
+                        Text(text = lang, fontWeight = FontWeight.Normal, fontSize = 12.sp)
+                    }
                 }
             }
         }
-    }
 
-    Column(
-        modifier = Modifier.padding(paddingValues).fillMaxWidth().padding(vertical = 205.dp, horizontal = 20.dp)
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth()
-        ){
-            Text(text = "Популярное", fontWeight = FontWeight.Normal, fontSize = 16.sp)
-            Box (
-
-            ){
-                Text(text = "Все", fontSize = 12.sp, color = MatuleTheme.colors.accent, modifier = Modifier.padding(top = 3.5.dp, start = 258.dp))
-            }
-        }
-
-        Row(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            ProductItem(
-                title = "Best Seller",
-                name = "Nike Air Max",
-                price = "₽752",
-                imageRes = painterResource(R.drawable.nadejda),
-                onClick = {},
-            )
-
-            Column(modifier = Modifier.fillMaxHeight().width(51.dp)) {}
-
-            ProductItem(
-                title = "Best Seller",
-                name = "Nike Air Max",
-                price = "₽752",
-                imageRes = painterResource(R.drawable.nadejda),
-                onClick = {},
-            )
-        }
-
-        Column (
-            modifier = Modifier.padding(paddingValues).fillMaxWidth().padding(vertical = 320.dp, horizontal = 20.dp)
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp)
         ) {
             Row(
                 modifier = Modifier.fillMaxWidth()
             ){
-                Text(text = "Акции", fontWeight = FontWeight.Medium, fontSize = 16.sp)
+                Text(text = "Популярное", fontWeight = FontWeight.Normal, fontSize = 16.sp)
                 Box (
 
                 ){
                     Text(text = "Все", fontSize = 12.sp, color = MatuleTheme.colors.accent, modifier = Modifier.padding(top = 3.5.dp, start = 258.dp))
+                }
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
+            ) {
+                ProductItem(
+                    title = "Best Seller",
+                    name = "Nike Air Max",
+                    price = "₽752",
+                    imageRes = painterResource(R.drawable.nadejda),
+                    onClick = {},
+                )
+
+                Column(modifier = Modifier.fillMaxHeight().width(51.dp)) {}
+
+                ProductItem(
+                    title = "Best Seller",
+                    name = "Nike Air Max",
+                    price = "₽752",
+                    imageRes = painterResource(R.drawable.nadejda),
+                    onClick = {},
+                )
+            }
+
+            Column (
+                modifier = Modifier.fillMaxWidth().padding(top = 31.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(bottom = 25.dp)
+                ){
+                    Text(text = "Акции", fontWeight = FontWeight.Medium, fontSize = 16.sp)
+                    Text(text = "Все", fontSize = 12.sp, color = MatuleTheme.colors.accent, modifier = Modifier.padding(top = 3.5.dp, start = 303.dp))
+                }
+
+                Box(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Image(painter = painterResource(
+                        R.drawable.rectangle_4231),
+                        contentDescription = "основав",
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    Image(
+                        painter = painterResource(R.drawable.sale),
+                        contentDescription = null,
+                        modifier = Modifier.align(Alignment.CenterStart)
+                            .padding(start = 22.dp)
+                    )
+
+                    Image(
+                        painter = painterResource(R.drawable.misc_06),
+                        contentDescription = null,
+                        modifier = Modifier.align(Alignment.Center)
+                            .padding(start = 60.dp, bottom = 30.dp)
+                    )
+
+                    Image(
+                        painter = painterResource(R.drawable.cale_sneakers),
+                        contentDescription = null,
+                        modifier = Modifier.align(Alignment.CenterEnd)
+                            .padding(end = 57.dp)
+                    )
                 }
             }
         }
